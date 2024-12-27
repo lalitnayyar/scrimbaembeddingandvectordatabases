746058ade5d172100c4b9ec8c343f3ddccdfe9ae
Lalit <lalitnayyar@gmail.com>
2024-12-28 01:21:26 +0400

    final after supabase dbscript and dbupload

--- splittextgetvectorsinsertintosupabase/index.js
+++ splittextgetvectorsinsertintosupabase/index.js
@@ -1,4 +1,6 @@
 import { openai, supabase } from './config.js';
+import fs from 'fs/promises';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 /*
   Challenge: Text Splitters, Embeddings, and Vector Databases!
@@ -10,13 +12,39 @@ import { openai, supabase } from './config.js';
 
 /* Split movies.txt into text chunks.
 Return LangChain's "output" – the array of Document objects. */
+
 async function splitDocument(document) {
-
+  const text = await fs.readFile(document, 'utf8');
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 250,
+    chunkOverlap: 15,
+  });
+  const output = await splitter.createDocuments([text]);
+  console.log(output);
+  return output;
 }
-
 /* Create an embedding from each text chunk.
 Store all embeddings and corresponding text in Supabase. */
 async function createAndStoreEmbeddings() {
-  // const chunkData = await splitDocument("movies.txt");
-  
+  const chunkData = await splitDocument( "movies.txt");
+  const data = await Promise.all(
+    chunkData.map(async (chunk) => {
+      const embeddingResponse = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        input: chunk.pageContent
+      });
+      return { 
+        content: chunk.pageContent, 
+        embedding: embeddingResponse.data[0].embedding 
+      }
+    })
+  );
+  await supabase.from('movies').insert(data);
+  console.log('SUCCESS!');
 }
+createAndStoreEmbeddings()
+
+
+
+
+
