d07127766d5e5838bb36879b1593b090e70fa24e
Lalit <lalitnayyar@gmail.com>
2024-12-27 21:40:45 +0400

    multiple data embeddings

--- createanembedding/index.js
+++ createanembedding/index.js
@@ -1,12 +1,19 @@
 import openai from './config.js';
 
+const content = [
+  "Beyond Mars: speculating life on distant planets.",
+  "Jazz under stars: a night in New Orleans' music scene.",
+  "Mysteries of the deep: exploring uncharted ocean caves.",
+  "Rediscovering lost melodies: the rebirth of vinyl culture.",
+  "Tales from the tech frontier: decoding AI ethics.",
+]; 
+
 /** Create embeddings representing the input text */
 async function main() {
   const embedding = await openai.embeddings.create({
     model: "text-embedding-ada-002",
-    input: "Hello, world!",
+    input: content,
   });
-  console.log(embedding.data[0].embedding);
+  console.log(embedding.data);
 }
 main();
-
