2e860ec957fc14408294b5fea9171569effe7495
Lalit <lalitnayyar@gmail.com>
2024-12-28 01:46:50 +0400

    added error handling

--- splittextgetvectorsinsertintosupabase/index.js
+++ splittextgetvectorsinsertintosupabase/index.js
@@ -14,33 +14,49 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 Return LangChain's "output" – the array of Document objects. */
 
 async function splitDocument(document) {
-  const text = await fs.readFile(document, 'utf8');
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 250,
-    chunkOverlap: 15,
-  });
-  const output = await splitter.createDocuments([text]);
-  console.log(output);
-  return output;
+  try {
+    const text = await fs.readFile(document, 'utf8');
+        // Check if fetch request was successful
+        // if (!text.ok) {
+        //   throw new Error('Network response was not ok.');
+        // }
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 250,
+      chunkOverlap: 15,
+    });
+    const output = await splitter.createDocuments([text]);
+    console.log(output);
+    return output;
+  } catch (err) {
+    console.error('There was an issue with splitting text');
+    throw err;
+  }
 }
 /* Create an embedding from each text chunk.
 Store all embeddings and corresponding text in Supabase. */
 async function createAndStoreEmbeddings() {
-  const chunkData = await splitDocument( "movies.txt");
-  const data = await Promise.all(
-    chunkData.map(async (chunk) => {
-      const embeddingResponse = await openai.embeddings.create({
-        model: "text-embedding-ada-002",
-        input: chunk.pageContent
-      });
-      return { 
-        content: chunk.pageContent, 
-        embedding: embeddingResponse.data[0].embedding 
-      }
-    })
-  );
-  await supabase.from('movies').insert(data);
-  console.log('SUCCESS!');
+  try {
+    const chunkData = await splitDocument( "movies.txt");
+    const data = await Promise.all(
+      chunkData.map(async (chunk) => {
+        const embeddingResponse = await openai.embeddings.create({
+          model: "text-embedding-ada-002",
+          input: chunk.pageContent
+        });
+        return { 
+          content: chunk.pageContent, 
+          embedding: embeddingResponse.data[0].embedding 
+        }
+      })
+    );
+    const { error } = await supabase.from('movies').insert(data);
+    if (error) {
+      throw new Error('Issue inserting data into the database.');
+    }
+    console.log('SUCCESS!');
+  } catch (err) {
+    console.error('ERROR: ' + err.message);
+  }
 }
 createAndStoreEmbeddings()
 
